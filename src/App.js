ea374bae90b18600f3d7a6cc18e4a02a4432ea46
dmcgeown <dmcgeown@acquisbi.com>
2021-06-02 09:31:38 +0100

    Mkaing minor change to test CI

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>Hi there - second edition!!!!</p>
+                <p>Hi there - third edition!!!!</p>
             </header>
         </div>
     );
